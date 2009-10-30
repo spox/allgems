5ad48ad8b56b9fd29cabec1911a4685583c925b0
spox <spox@modspox.com>
2009-10-30 09:40:40 -0700

    add database holder

--- lib/allgems.rb
+++ lib/allgems.rb
@@ -1,11 +1,12 @@
 module AllGems
     class << self
-        attr_accessor :data_directory, :rdoc_format, :rdoc_version, :logger
+        attr_accessor :data_directory, :rdoc_format, :rdoc_version, :logger, :db
         def defaulterize
             @data_directory = nil
             @rdoc_format = 'darkfish'
             @rdoc_version = '2.3.0' # hanna is dependent on this
             @logger = nil
+            @db = nil
         end
         # db: Sequel::Database
         # Run any migrations needed
@@ -13,6 +14,7 @@ module AllGems
         def initialize_db(db)
             require 'sequel/extensions/migration'
             Sequel::Migrator.apply(db, "#{File.expand_path(__FILE__).gsub(/\/[^\/]+$/, '')}/allgems/migrations")
+            self.db = db
         end
     end
 end
